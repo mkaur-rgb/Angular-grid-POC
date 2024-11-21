f91ed1b5730b4c17d57dee2da9b4dc4a5d021335
mkaur-rgb <manpreetkaur6311062@gmail.com>
2024-11-21 01:00:31 -0500

    Features developed for Pagination, Theme, Row Edit, Filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
-import { ColDef } from 'ag-grid-community';
+import { ColDef,RowSelectionOptions,createGrid } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
 /* Quartz Theme Specific CSS */
 import 'ag-grid-community/styles/ag-theme-quartz.css';
@@ -11,37 +11,332 @@ import 'ag-grid-community/styles/ag-theme-quartz.css';
   standalone: true,
   imports: [AgGridAngular], // Add Angular Data Grid Component
   styleUrls: ['./app.component.scss'],
-  template: `
+  template: `<div style="height: 100%; box-sizing: border-box;">
   <ag-grid-angular
-    [rowData]="rowData"
+    [rowData]="rowData"  [defaultColDef]="defaultColDef"
+    [rowSelection]="rowSelection"
     [columnDefs]="colDefs" [pagination]="pagination"
     [paginationPageSize]="paginationPageSize"
     [paginationPageSizeSelector]="paginationPageSizeSelector" [class]="themeClass"
- style="height: 500px;"/>`
+ style="height: 500px;"/></div>`
 })
 export class AppComponent {
   title = 'AgGrid';
-  pagination= true;
-  paginationPageSize = 50;
-  paginationPageSizeSelector= [200,500,1000]
 
- ngOninit(){
-  this.pagination= true;
-  this.paginationPageSize = 50;
-  this.paginationPageSizeSelector= [200,500,1000]
- }
-  public themeClass: string = "ag-theme-quartz";
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  public pagination = true;
+  public paginationPageSize = 10;
+  public paginationPageSizeSelector: number[] | boolean = [10, 25, 50];
+  public themeClass: string =
+    "ag-theme-quartz-dark";
+
+  public gridDiv = document.querySelector<HTMLElement>("#myGrid")!;
+  public rowData: any[] | null = [
+    {
+      make: "Tesla",
+      model: "Model Y",
+      price: 64950,
+      electric: true,
+      month: "June",
+    },
+    {
+      make: "Ford",
+      model: "F-Series",
+      price: 33850,
+      electric: false,
+      month: "October",
+    },
+    {
+      make: "Toyota",
+      model: "Corolla",
+      price: 29600,
+      electric: false,
+      month: "August",
+    },
+    {
+      make: "Mercedes",
+      model: "EQA",
+      price: 48890,
+      electric: true,
+      month: "February",
+    },
+    {
+      make: "Fiat",
+      model: "500",
+      price: 15774,
+      electric: false,
+      month: "January",
+    },
+    {
+      make: "Nissan",
+      model: "Juke",
+      price: 20675,
+      electric: false,
+      month: "March",
+    },
+    {
+      make: "Vauxhall",
+      model: "Corsa",
+      price: 18460,
+      electric: false,
+      month: "July",
+    },
+    {
+      make: "Volvo",
+      model: "EX30",
+      price: 33795,
+      electric: true,
+      month: "September",
+    },
+    {
+      make: "Mercedes",
+      model: "Maybach",
+      price: 175720,
+      electric: false,
+      month: "December",
+    },
+    {
+      make: "Vauxhall",
+      model: "Astra",
+      price: 25795,
+      electric: false,
+      month: "April",
+    },
+    {
+      make: "Fiat",
+      model: "Panda",
+      price: 13724,
+      electric: false,
+      month: "November",
+    },
+    {
+      make: "Jaguar",
+      model: "I-PACE",
+      price: 69425,
+      electric: true,
+      month: "May",
+    },
+    {
+      make: "Tesla",
+      model: "Model Y",
+      price: 64950,
+      electric: true,
+      month: "June",
+    },
+    {
+      make: "Ford",
+      model: "F-Series",
+      price: 33850,
+      electric: false,
+      month: "October",
+    },
+    {
+      make: "Toyota",
+      model: "Corolla",
+      price: 29600,
+      electric: false,
+      month: "August",
+    },
+    {
+      make: "Mercedes",
+      model: "EQA",
+      price: 48890,
+      electric: true,
+      month: "February",
+    },
+    {
+      make: "Fiat",
+      model: "500",
+      price: 15774,
+      electric: false,
+      month: "January",
+    },
+    {
+      make: "Nissan",
+      model: "Juke",
+      price: 20675,
+      electric: false,
+      month: "March",
+    },
+    {
+      make: "Vauxhall",
+      model: "Corsa",
+      price: 18460,
+      electric: false,
+      month: "July",
+    },
+    {
+      make: "Volvo",
+      model: "EX30",
+      price: 33795,
+      electric: true,
+      month: "September",
+    },
+    {
+      make: "Mercedes",
+      model: "Maybach",
+      price: 175720,
+      electric: false,
+      month: "December",
+    },
+    {
+      make: "Vauxhall",
+      model: "Astra",
+      price: 25795,
+      electric: false,
+      month: "April",
+    },
+    {
+      make: "Fiat",
+      model: "Panda",
+      price: 13724,
+      electric: false,
+      month: "November",
+    },
+    {
+      make: "Jaguar",
+      model: "I-PACE",
+      price: 69425,
+      electric: true,
+      month: "May",
+    },
+    {
+      make: "Tesla",
+      model: "Model Y",
+      price: 64950,
+      electric: true,
+      month: "June",
+    },
+    {
+      make: "Ford",
+      model: "F-Series",
+      price: 33850,
+      electric: false,
+      month: "October",
+    },
+    {
+      make: "Toyota",
+      model: "Corolla",
+      price: 29600,
+      electric: false,
+      month: "August",
+    },
+    {
+      make: "Mercedes",
+      model: "EQA",
+      price: 48890,
+      electric: true,
+      month: "February",
+    },
+    {
+      make: "Fiat",
+      model: "500",
+      price: 15774,
+      electric: false,
+      month: "January",
+    },
+    {
+      make: "Nissan",
+      model: "Juke",
+      price: 20675,
+      electric: false,
+      month: "March",
+    },
+    {
+      make: "Vauxhall",
+      model: "Corsa",
+      price: 18460,
+      electric: false,
+      month: "July",
+    },
+    {
+      make: "Volvo",
+      model: "EX30",
+      price: 33795,
+      electric: true,
+      month: "September",
+    },
+    {
+      make: "Mercedes",
+      model: "Maybach",
+      price: 175720,
+      electric: false,
+      month: "December",
+    },
+    {
+      make: "Vauxhall",
+      model: "Astra",
+      price: 25795,
+      electric: false,
+      month: "April",
+    },
+    {
+      make: "Fiat",
+      model: "Panda",
+      price: 13724,
+      electric: false,
+      month: "November",
+    },
+    {
+      make: "Jaguar",
+      model: "I-PACE",
+      price: 69425,
+      electric: true,
+      month: "May",
+    },
   ];
-
-  // Column Definitions: Defines the columns to be displayed.
-  colDefs: ColDef[] = [
-    { field: "make" },
+  public colDefs: ColDef[] = [
+    {
+      field: "make",
+      editable: true,
+      cellEditor: "agSelectCellEditor",
+      cellEditorParams: {
+        values: [
+          "Tesla",
+          "Ford",
+          "Toyota",
+          "Mercedes",
+          "Fiat",
+          "Nissan",
+          "Vauxhall",
+          "Volvo",
+          "Jaguar",
+        ],
+      },
+    },
     { field: "model" },
-    { field: "price" },
-    { field: "electric" }
+    { field: "price", filter: "agNumberColumnFilter" },
+    { field: "electric" },
+    {
+      field: "month",
+      comparator: (valueA, valueB) => {
+        const months = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ];
+        const idxA = months.indexOf(valueA);
+        const idxB = months.indexOf(valueB);
+        return idxA - idxB;
+      },
+    },
   ];
+  public defaultColDef: ColDef = {
+    filter: "agTextColumnFilter",
+    floatingFilter: true,
+  };
+  public rowSelection: RowSelectionOptions | "single" | "multiple" = {
+    mode: "multiRow",
+    headerCheckbox: false,
+  };
+
 }
+
